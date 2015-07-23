ba7596c124e76a3b3d7c1ecaa9c756c344d43155
Mikhail Domrachev <mikhail.domrachev.imit@yandex.ru>
2015-07-23 19:21:01 +0600

    Hello Apple

--- CocoaTest/ViewController.m
+++ CocoaTest/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSLog(@"Acer hello");
+    NSLog(@"Apple hello");
     // Do any additional setup after loading the view, typically from a nib.
 }
 
